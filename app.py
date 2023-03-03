d57edb732b9d159388c9e5036a9ccb96e7dd23df
ksaxit <exit_s_no@mail.ru>
2023-03-03 22:48:30 +0300

    Ready

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    secondname = db.Column(db.String(300), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    about_you = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+
+@app.route('/from_here')
+@app.route('/here')
+def here():
+    return render_template('index.html')
+
+
+
+@app.route('/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/Piatnica')
+def piatnica():
+    return render_template('Piatnica.html')
+
+
+@app.route('/Dmitri')
+def dmitri():
+    return render_template('Dmitriy.html')
+
+
+@app.route('/Stef')
+def stef():
+    return render_template('Stef.html')
+
+
+@app.route('/create')
+def create():
+    article = Article.query.order_by(Article.date.desc()).all()
+    return render_template('create.html', article=article)
+
+
+@app.route('/create/<int:id>')
+def create_detail(id):
+    article = Article.query.get(id)
+    return render_template('create_detail.html', article=article)
+
+
+@app.route('/create/<int:id>/del')
+def create_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/create')
+    except:
+        return 'При удалении произошла ошибка'
+
+
+@app.route('/create/<int:id>/update', methods=["POST", "GET"])
+def create_update(id):
+    article = Article.query.get(id)
+
+    if request.method == "POST":
+        article.firstname = request.form['firstname']
+        article.secondname = request.form['secondname']
+        article.username = request.form['username']
+        article.about_you = request.form['about_you']
+        try:
+            db.session.commit()
+            return redirect('/create')
+        except:
+            return "При редактировании профиля возникла ошибка"
+
+    else:
+        return render_template('create_update.html', article=article)
+
+
+@app.route('/create-article', methods=["POST", "GET"])
+def create_article():
+    if request.method == "POST":
+        firstname = request.form['firstname']
+        secondname = request.form['secondname']
+        username = request.form['username']
+        about_you = request.form['about_you']
+        article = Article(firstname=firstname, secondname=secondname, username=username, about_you=about_you)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/create')
+        except:
+            return "При добавлении профиля возникла ошибка"
+
+    else:
+        return render_template('create_article.html')
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
